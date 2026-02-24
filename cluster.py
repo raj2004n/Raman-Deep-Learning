60ed694decc4c177963f6e63a8f0cc3d51e43e3d
Raj Negi <raj2004n@gmail.com>
2026-02-24 13:11:45 +0000

    testing methods for clusters and unmixing

---
+++ cluster.py
@@ -0,0 +1,32 @@
+from raman_helper import *
+from matplotlib.widgets import TextBox
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+path = Path("~/Code/Data_SH/SB008").expanduser()
+raman_data = Raman_Data(path, 10, 13)
+
+cell_layer = raman_data.get_raw_slices()
+
+preprocessing_pipeline = rp.preprocessing.Pipeline([
+    rp.preprocessing.misc.Cropper(region=(150, 1200))
+])
+
+preprocessed_cell_layer = preprocessing_pipeline.apply(cell_layer)
+
+kmeans = rp.analysis.cluster.KMeans(n_clusters=3)
+
+clusters, cluster_centres = kmeans.apply(preprocessed_cell_layer)
+
+rp.plot.spectra(
+    cluster_centres, preprocessed_cell_layer.spectral_axis,
+    plot_type="single stacked",
+    label=[f"Cluster centre {i + 1}" for i in range(len(cluster_centres))]
+    )
+plt.show()
+
+rp.plot.image(
+    clusters, title=[f"Clusters {i + 1}" for i in range(len(clusters))],
+    cbar=False
+    )
+plt.show()
---
+++ unmix.py
@@ -0,0 +1,34 @@
+from raman_helper import *
+from matplotlib.widgets import TextBox
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+path = Path("~/Code/Data_SH/SB008").expanduser()
+raman_data = Raman_Data(path, 10, 13)
+
+cell_layer = raman_data.get_raw_slices()
+
+preprocessing_pipeline = rp.preprocessing.Pipeline([
+    rp.preprocessing.misc.Cropper(region=(150, 1200))
+])
+
+preprocessed_cell_layer = preprocessing_pipeline.apply(cell_layer)
+
+#nfindr = rp.analysis.unmix.NFINDR(n_endmembers=4, abundance_method='fcls')
+#abundance_maps, endmembers = nfindr.apply(preprocessed_cell_layer)
+
+fippi = rp.analysis.unmix.VCA(n_endmembers=3, abundance_method='nnls')
+abundance_maps, endmembers = fippi.apply(preprocessed_cell_layer)
+
+rp.plot.spectra(
+    endmembers, preprocessed_cell_layer.spectral_axis, 
+    plot_type="single stacked", 
+    label=[f"Endmember {i + 1}" for i in range(len(endmembers))]
+    )
+plt.show()
+
+rp.plot.image(abundance_maps,
+              title=[f"Component {i + 1}" for i in range(len(abundance_maps))]
+              )
+
+plt.show()
